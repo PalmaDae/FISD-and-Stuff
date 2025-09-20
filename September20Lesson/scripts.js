0cd2b0cc9e6a5c00f83b9d231bd06deff3ab8ea3
palmadae <arturd277@mail.ru>
2025-09-20 16:36:22 +0300

    some js

--- September20Lesson/scripts.js
+++ September20Lesson/scripts.js
@@ -1,5 +1,16 @@
 function changeToRight() {
     let block = document.getElementById("dev");
-    block.classList.remove("left");
-    block.classList.add("right");
+    let id = document.getElementById("id_text");
+    let idButton = document.getElementById("id_button");
+    if (block.classList == "left") {
+        block.classList.remove("left");
+        block.classList.add("right");
+        id.textContent = "Надпись справа";
+        idButton.textContent = "Правая кнопка";
+    } else {
+        block.classList.remove("right");
+        block.classList.add("left");
+        id.textContent = "Надпись слева";
+        idButton.textContent = "Левая кнопка";
+    }
 }
