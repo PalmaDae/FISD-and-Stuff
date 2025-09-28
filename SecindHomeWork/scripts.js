d14c6d07a956ef74e0ba07f9032af7abaf780819
palmadae <arturd277@mail.ru>
2025-09-28 17:19:02 +0300

    start work

---
+++ SecindHomeWork/scripts.js
@@ -0,0 +1,27 @@
+function randomFact() {
+
+}
+
+function changeTheme() {
+
+}
+
+function picturesCarousel() {
+
+}
+
+function feedBack() {
+
+}
+
+function beatifulInfo() {
+
+}
+
+function timer() {
+
+}
+
+function trueSideOfPage() {
+    
+}
