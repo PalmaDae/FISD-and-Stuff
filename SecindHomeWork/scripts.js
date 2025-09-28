4a54b8597f19843b0b4b34e92fb4a162104ef2db
Arthur <arturd277@mail.ru>
2025-09-28 17:56:23 +0300

    theme is working

--- SecindHomeWork/scripts.js
+++ SecindHomeWork/scripts.js
@@ -1,19 +1,32 @@
+let block;
+
 function randomFact() {
 
 }
 
 function changeTheme() {
-    let block = document.getElementById("change_theme");
     let idButton = document.getElementById("changeButton");
 
-    if (block.classList == "radiant") {
+    if (block.classList.contains("radiant")) {
         block.classList.remove("radiant");
         block.classList.add("dark");
+        localStorage.setItem('theme', 'dark');
     } else {
         block.classList.remove("dark");
         block.classList.add("radiant");
+        localStorage.setItem('theme', 'radiant');
     }
 }
+
+window.addEventListener('load', () => {
+    block = document.getElementById("change_theme");
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+        block.classList.remove('radiant', 'dark');
+        block.classList.add(savedTheme);
+    }
+});
 
 function picturesCarousel() {
 
