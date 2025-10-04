5b7c7e157b52d5640130e85085f63d2be4f513e5
palmadae <arturd277@mail.ru>
2025-10-04 17:02:34 +0300

    servlets and gags

--- src/ThirdHomeWork/FirstServlet.java
+++ src/ThirdHomeWork/FirstServlet.java
@@ -2,9 +2,7 @@ package ThirdHomeWork;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 
 import java.io.*;
 
@@ -15,8 +13,6 @@ public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-
-
 
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("someone.html");
 
