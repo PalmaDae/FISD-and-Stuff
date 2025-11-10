5416d969d87301a4045983c31700a40cdc25ac31
PalmaDae <arturd277@mail.ru>
2025-11-10 13:16:54 +0300

    createCharacter

--- src/main/java/dao/CharakterDAO.java
+++ src/main/java/dao/CharakterDAO.java
@@ -14,17 +14,6 @@ public class CharakterDAO {
         this.connection = connection;
     }
 
-
-    public void createUser(String username, String pass) throws SQLException {
-        String sql = "insert into users (username, hash_pass, role) values (?, ?, ?)";
-        try(PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setString(1, username);
-            ps.setString(2, pass);
-            ps.setString(3, Role.USER.name());
-            ps.executeUpdate();
-        }
-    }
-
     public void createCharacter(Charakter charakter) throws SQLException {
         String sql = "insert into characters (" +
                 "name, player_name, race, clasz, level, " +
@@ -37,6 +26,24 @@ public class CharakterDAO {
             ps.setString(1, charakter.getName());
             ps.setString(2, charakter.getPlayerName());
             ps.setString(3, charakter.getRace().name());
+            ps.setString(4, charakter.getClasz().name());
+            ps.setInt(5, charakter.getLevel());
+            ps.setInt(6, charakter.getStrength());
+            ps.setInt(7, charakter.getIntelligence());
+            ps.setInt(8, charakter.getWisdom());
+            ps.setInt(9, charakter.getCharisma());
+            ps.setInt(10, charakter.getDexterity());
+            ps.setInt(11, charakter.getConstitution());
+            ps.setInt(12, charakter.getSpeed());
+            ps.setInt(13, charakter.getArmorClass());
+            ps.setInt(14, charakter.getInitiative());
+            ps.setInt(15, charakter.getHitPoints());
+            ps.setInt(16, charakter.getTemporaryHitPoints());
+            ps.setInt(17, charakter.getHitDice());
+            ps.setInt(18, charakter.getGold());
+            ps.setInt(19, charakter.getSilver());
+            ps.setInt(20, charakter.getCopper());
+            ps.executeUpdate();
         }
     }
 }
